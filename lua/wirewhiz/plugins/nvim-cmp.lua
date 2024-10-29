92c0b579105ae27733e1c2c416c8db4c0f357cee
WireWhiz <eli@knisley.info>
2024-10-29 18:25:30 -0400

    fix autocomplete tab direction

--- lua/wirewhiz/plugins/nvim-cmp.lua
+++ lua/wirewhiz/plugins/nvim-cmp.lua
@@ -24,8 +24,8 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                ["<S-Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
